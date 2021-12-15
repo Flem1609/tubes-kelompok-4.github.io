d62ab33dddf1c2f0330d68801032b0fce819bff4
FairuzArifin <89765817+FairuzArifin@users.noreply.github.com>
2021-12-15 15:18:43 +0700

    Add files via upload

---
+++ Source Code/Profil/tentangkami.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Ganti title nya dengan nama masakannya -->
+    <title>Tentang Kami</title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+	  <link rel="stylesheet" href="Sidebar/css/style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+    </style>
+    <style>
+
+      .social-link {
+        width: 30px;
+        height: 30px;
+        border: 1px solid #ddd;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #666;
+        border-radius: 50%;
+        transition: all 0.3s;
+        font-size: 0.9rem;
+      }
+
+      .social-link:hover, .social-link:focus {
+        background: #ddd;
+        text-decoration: none;
+        color: #555;
+      }
+      
+    </style>
+</head>
+<body style="background-color: rgb(235, 235, 235);">
+
+    <?php 
+	    session_start();
+	    if($_SESSION['level']==""){
+		    header("location:../Akun/login.php?pesan=gagal");
+	    }
+    ?>
+
+   <!-- Sidebar --> 
+
+    <div class="wrapper d-flex align-items-stretch">
+    <nav id="sidebar">
+      <div class="p-4 pt-5">
+      <a href="#" class="img logo rounded-circle mb-3" style="background-image: url(https://img.pikbest.com/png-images/qiantu/hand-drawn-noodle-restaurant-chef-chef-illustration_2720597.png!c1024wm0/compress/true/progressive/true/format/webp/fw/1024);"></a>
+          <center><span><b><?php echo $_SESSION['nama'] ?></b></span></center>
+          <ul class="list-unstyled components mb-5 mt-3">
+            <li>
+              <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">List Makanan</a>
+            <ul class="collapse list-unstyled" id="homeSubmenu">
+            <li>
+                <a href="../Resep/cari1.php">Masakan Sumatera dan Jawa</a>
+              </li>
+              <li>
+                <a href="../Resep/cari2.php">Masakan Kalimantan dan Sulawesi</a>
+              </li>
+              <li>
+                <a href="../Resep/cari3.php">Masakan Maluku dan Papua</a>
+              </li>
+            </ul>
+            </li>
+            <li class="active">
+              <a href="tentangkami.php">Tentang Kami</a>
+            </li>
+            <li>
+              <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Akun</a>
+            <ul class="collapse list-unstyled" id="pageSubmenu">
+              <li>
+                <a href="../Akun/logout.php">Keluar</a>
+              </li>
+              </ul>
+            </li>
+            <li>
+              <a class="disabled">Hubungi Kami</a>
+            </li>
+            <li>
+              <a href="../Saran/saran.php">Saran dan Kritikan</a>
+            </li>
+          </ul>
+      </div>
+    </nav>
+
+    <div id="content" class="p-4 p-md-5">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <button type="button" id="sidebarCollapse" class="btn btn-primary">
+            <i class="fa fa-bars"></i>
+            <span class="sr-only">Toggle Menu</span>
+          </button>
+          <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <i class="fa fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav navbar-nav ml-auto">
+              <li class="nav-item active">
+                  <a class="nav-link" href="../index.php">Halaman Utama</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="tentangkami.php">Tentang Kami</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link disabled" href="#">Hubungi</a>
+              </li>
+            </ul>
+        </div>
+      </nav>
+
+
+      <div class="bg-light">
+        <div class="container py-5">
+          <div class="row h-100 align-items-center py-5">
+            <div class="col-lg-6">
+              <h1 class="display-4">Tentang Website kami</h1>
+              <p class="lead text-muted mb-0">Kami Kelompok 4 dengan bangga mempersembahkan hasil dari kerja kami berupa web resep makanan. Mohon maaf jika ada kesalahan dalam penulisan dan kesalahan lainnya. Terima kasih</p>
+              </p>
+            </div>
+            <div class="col-lg-6 d-none d-lg-block"><img src="logo2.png" alt="" class="img-fluid"></div>
+          </div>
+        </div>
+      </div>
+
+      
+      <div class="bg-light py-5">
+        <div class="container py-5">
+          <div class="row mb-4">
+            <div class="col-lg-5">
+              <h2 class="display-4 font-weight-light">Our team</h2>
+            </div>
+          </div>
+      
+          <div class="row text-center">
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="fairuz.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Fairuz</h5><span class="small text-uppercase text-muted">Ketua Kelompok</span>
+              </div>
+            </div>
+            <!-- End-->
+      
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="dhani.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Dhani</h5><span class="small text-uppercase text-muted">Anggota Kelompok</span>
+              </div>
+            </div>
+            <!-- End-->
+      
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="david.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">David</h5><span class="small text-uppercase text-muted">Anggota Kelompok</span>
+              </div>
+            </div>
+            <!-- End-->
+      
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="fleming.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Fleming</h5><span class="small text-uppercase text-muted">Anggota Kelompokr</span>
+              </div>
+            </div>
+            <!-- End-->
+
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="sofie.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Sofie</h5><span class="small text-uppercase text-muted">Anggota Kelompok</span>
+              </div>
+            </div>
+            <!-- End-->
+
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="recindy.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Recindy</h5><span class="small text-uppercase text-muted">Anggota Kelompok</span>
+              </div>
+            </div>
+            <!-- End-->
+      
+          </div>
+        </div>
+      </div>    
+    </div>
+    </div>
+    
+
+    <!-- Footer -->
+
+    <section style="background-color: #000000;">
+        <div style="background-color: #ff9900;">
+          <div class="container">
+            <div class="row py-4 d-flex align-items-center">
+              <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0" style="color: white;">
+                <h6 class="mb-0">Ayo bergabung dengan media sosial kami</h6>
+              </div>
+              <div class="col-md-6 col-lg-7 text-center text-md-right">
+                <a class="fb-ic">
+                  <i class="fab fa-facebook-f white-text mr-4"> </i>
+                </a>
+                <a class="tw-ic">
+                  <i class="fab fa-twitter white-text mr-4"> </i>
+                </a>
+                <a class="gplus-ic">
+                  <i class="fab fa-google-plus-g white-text mr-4"> </i>
+                </a>
+                <a class="li-ic">
+                  <i class="fab fa-linkedin-in white-text mr-4"> </i>
+                </a>
+                <a class="ins-ic">
+                  <i class="fab fa-instagram white-text"> </i>
+                </a>
+              </div>      
+            </div>      
+          </div>
+        </div>
+        <div class="container text-center text-md-left mt-5" style="color: white;">
+          <div class="row mt-3">
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <h6 class="text-uppercase font-weight-bold">Tugas Akhir Kelompok 4</h6>
+              <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+              <p>Kami dengan bangga mempersembahkan hasil dari kerja kami berupa web resep makanan. Mohon maaf jika ada kesalahan dalam penulisan dan kesalahan lainnya. Terima kasih</p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 class="text-uppercase font-weight-bold">Resep</h6>
+              <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+              <p>
+                <a href="../Resep/cari.php">Makanan Tradisional</a>
+              </p>
+              <p>
+                <a href="#!" class="disabled">Makanan Modern<br>( Dalam Pengerjaan )</a>
+              </p>
+            </div>
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 class="text-uppercase font-weight-bold">Link Singkat</h6>
+              <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+              <p>
+                <a href="../Akun/logout.php">Keluar</a>
+              </p>
+              <p>
+                <a href="../Saran/saran.php">Kolom Saran</a>
+              </p>
+              <p>
+                <a class="disabled">Menjadi Distributor Resep</a>
+              </p>
+              <p>
+                <a class="disabled">Bantuan</a>
+              </p>  
+            </div>
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <h6 class="text-uppercase font-weight-bold">Kontak</h6>
+              <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+              <p>
+                <i class="fas fa-home mr-3"></i> Medan, USU Fasilkom TI, Teknologi Informasi</p>
+              <p>
+                <i class="fas fa-envelope mr-3"></i> Kelompok4tubes@gmail.com</p>
+              <p>
+                <i class="fas fa-phone mr-3"></i> + 62 812 3456 7890</p>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright text-center py-3">© 2021 Copyright:
+          <a href="../index.html"> Kelompok 4 Tubes Pemrograman Web</a>
+        </div>
+    </section>
+
+    <!-- Script -->
+
+    <script src="Sidebar/js/jquery.min.js"></script>
+    <script src="Sidebar/js/popper.js"></script>
+    <script src="Sidebar/js/bootstrap.min.js"></script>
+    <script src="Sidebar/js/main.js"></script>
+    <script src="js/bootstrap.js"></script>
+  
+</body>
+</html>
